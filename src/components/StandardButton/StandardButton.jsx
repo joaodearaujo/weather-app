9dd8020e5d559298ad5c8e383dadde927e8a5b1a
João Pedro Araújo <joao.pedrocdearaujo@gmail.com>
2026-01-24 02:12:21 -0300

    feat: create StandardButton component

---
+++ src/components/StandardButton/StandardButton.jsx
@@ -0,0 +1,7 @@
+import './StandardButton.css'
+
+const StandardButton = ({buttonName}) => {
+    return  <button className='standard-button'>{buttonName}</button>
+}
+
+export default StandardButton;
